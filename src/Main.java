095ac9227a46e15efccdfa7692dc967829021252
Ramon Brand <ramon2009@gmail.com>
2021-05-15 11:01:58 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    Main main = new Main();
+    main.startServer();
+  }
+  private void startServer() {
+    System.out.println("Hello world 4");
+  }
+}
